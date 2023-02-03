25fe8abe76aa8ac68e3f1c060ff90b40ce713457
gabrrielsilva <gabrielsilvaev@gmail.com>
2023-02-02 22:10:15 -0300

    feat: href query selector by image and sync description

--- src/extractData.ts
+++ src/extractData.ts
@@ -1,12 +1,11 @@
 import { Page } from 'puppeteer';
 import { delay } from './utils/delay';
 
-export async function extractData(page: Page, keywords: string[], existingData: { id: string }[]) {
+export async function extractData(page: Page, keywords: string[], existingData: { id: string }[], groupUrl: string) {
   const links = await page.$$('a.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.xt0b8zv.xo1l8bm');
-  
   for await (const link of links) {
     try {
-      await link.evaluate(e => e.scrollIntoView({ block: "center", inline: "center" }));
+      await link.evaluate(e => e.scrollIntoView({ block: 'center', inline: 'center' }));
       await link.hover();
       await delay(2000);
     } catch {
@@ -14,26 +13,32 @@ export async function extractData(page: Page, keywords: string[], existingData: 
     }
   } // need to hover on the date time to load href
 
-  const data = await page.evaluate(async (keywords, existingData) => {    
+  const data = await page.evaluate(async (keywords, existingData, groupUrl) => {    
     const ads: { description: string, linkToAd: string }[] = [];
+    const posts = Array.from<HTMLElement>(document.querySelectorAll('div.x9f619.x1n2onr6.x1ja2u2z.x2bj2ny.x1qpq9i9.xdney7k.xu5ydu1.xt3gfkd.xh8yej3.x6ikm8r.x10wlt62.xquyuld')); // init 2
+    posts.splice(0, 2);
 
-    const adsData = <NodeListOf<HTMLDivElement>>document.querySelectorAll('div [class = "x11i5rnm xat24cr x1mh8g0r x1vvkbs xdj266r x126k92a"]');
-    const linksData = <NodeListOf<HTMLAnchorElement>>document.querySelectorAll('a.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.xt0b8zv.xo1l8bm');
-    for (let i = 0; i < adsData.length; i++) {
-      const description = adsData[i].innerText;
+    for await (const post of posts) {
+      const description = post.querySelector('div.x11i5rnm.xat24cr.x1mh8g0r.x1vvkbs.xdj266r') as HTMLDivElement;
+      const adLink = post.querySelector('a.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.xt0b8zv.xo1l8bm') as HTMLAnchorElement;
+      const photoLink = post.querySelector('a.x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1rg5ohu.x1a2a7pz.x1ey2m1c.xds687c.x10l6tqk.x17qophe.x13vifvy.x1pdlv7q') as HTMLAnchorElement;
+
       for await (const keyword of keywords) {
-        const formattedKeyword = keyword.toLowerCase().replace(/ /g, '').replace(/[^a-z0-9]/gi, '');
-        const formattedDescription = description.toLowerCase().replace(/ /g, '').replace(/[^a-z0-9]/gi, '');
-        const linkToAd = linksData[i].href.split('?')[0];
+        const formattedDescription = description?.innerText.toLowerCase().replace(/ /g, '').replace(/[^a-z0-9]/gi, '');
+        const linkToAd = adLink?.href.split('?')[0];
+        const linkToAd2 = photoLink?.href;
         
-        if (!existingData.find(data => data.id === formattedDescription) && formattedDescription.includes(formattedKeyword)) {          
-          ads.push({ description, linkToAd: <string>linkToAd });
+        if (
+          !existingData.find(data => data.id === formattedDescription) 
+          && description?.innerText?.toLowerCase().includes(keyword.toLowerCase())
+          && !ads.find(ad => ad.description === description?.innerText)
+        ) {          
+          ads.push({ description: description?.innerText || 'Sem descrição', linkToAd: <string>linkToAd === groupUrl.split('?')[0] ? linkToAd2 : linkToAd });
         }
       }
     }
-  
     return ads;
-  }, keywords, existingData);
+  }, keywords, existingData, groupUrl);
 
   return data;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import puppeteer from 'puppeteer';
+import puppeteer, { Browser, BrowserContext, Page } from 'puppeteer';
 import qrCode from 'qrcode-terminal';
 import { Client } from 'whatsapp-web.js';
 import { extractData } from './extractData';
@@ -12,51 +12,48 @@ import { delay } from './utils/delay';
 import { verifyAuthentication } from './verifyAuthentication';
 dotenv.config();
 
+let groupIndex = 0;
 const GROUPS = [
-  process.env.FACEBOOK_GROUP_URL_1,
-  process.env.FACEBOOK_GROUP_URL_2,
-  process.env.FACEBOOK_GROUP_URL_3,
-  process.env.FACEBOOK_GROUP_URL_4,
+  <string>process.env.FACEBOOK_GROUP_URL_1,
+  <string>process.env.FACEBOOK_GROUP_URL_2,
+  <string>process.env.FACEBOOK_GROUP_URL_3,
+  <string>process.env.FACEBOOK_GROUP_URL_4,
 ]
 
-const client = new Client({
-  puppeteer: {
-    args: ['--no-sandbox','--disable-setuid-sandbox']
-  }
-});
-
+const client = new Client({ puppeteer: { args: ['--no-sandbox','--disable-setuid-sandbox'] }});
 client.on('qr', qr => qrCode.generate(qr, { small: true }));
-client.on('ready', async () => { await import('./checkIncomingMessageChatId') });
+client.on('ready', async () => {await import('./checkIncomingMessageChatId'); run(['butter', '30', '34', '35', 'OFF', 'vendo']) });
 client.initialize();
 
-let groupIndex = 0;
+let browser: Browser;
+let page: Page;
+let context: BrowserContext;
+
+(async () => {
+  browser = await puppeteer.launch({ headless: true,  args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  page = await browser.newPage();
+  context = browser.defaultBrowserContext();
+})()
+
 export async function run (keywords: string[]) {
-  const browser = await puppeteer.launch({ headless: true,  args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-  const page = await browser.newPage();
-  const context = browser.defaultBrowserContext();
-
-  async function makeScraping(keywords: string[]) {
-    await overridePermissions(context);
-    await verifyAuthentication(page);
-    await goToFacebookGroup(page, <string>GROUPS[groupIndex]);
-    await autoScroll(page);
-    const existingData = await getExistingData();
-    const data = await extractData(page, keywords, existingData);
-    if (!data) {
-      await delay(1000 * 60 * 1); //1 min
-      makeScraping(keywords);
-    }
-    await registerAds(data);
-    for await (const ad of data) {
-      client.sendMessage(<string>process.env.CHAT_ID, `*Descrição:* ${ad.description}\n\n*Anúncio:* ${ad.linkToAd}`);
-    }
-    groupIndex++;
-    if (groupIndex === GROUPS.length - 1) groupIndex = 0;
-    await delay(1000 * 60 * 1); //1 min
-    makeScraping(keywords);
+  await overridePermissions(context);
+  await verifyAuthentication(page);
+  await goToFacebookGroup(page, GROUPS[groupIndex]);
+  await autoScroll(page);
+  const existingData = await getExistingData();
+  const data = await extractData(page, keywords, existingData, GROUPS[groupIndex]);
+  if (!data) {
+    await delay(1000 * 30); //30s
+    run(keywords);
+  };
+  await registerAds(data);
+  for await (const ad of data) {
+    client.sendMessage(<string>process.env.CHAT_ID, `*Descrição:* ${ad.description}\n\n*Anúncio:* ${ad.linkToAd}`);
   }
-
-  makeScraping(keywords);
+  groupIndex++;
+  if (groupIndex === GROUPS.length - 1) groupIndex = 0;
+  await delay(1000 * 30); //30s
+  run(keywords);
 }
 
 export { client };
